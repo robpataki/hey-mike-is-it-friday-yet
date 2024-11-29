29086269e53259669bacbd4de9dc781bf1a131f2
Rob Pataki <rob.pataki@icloud.com>
2024-11-29 16:50:13 +0000

    Update page description text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hey Mike, is it Friday yet?",
   description:
-    "Mike's t-shirt based calendar is more reliable than any calendar in the history of mankind. Our community relies on Mike's YODA t-shirt to show us the way and reduce our anxiety levels. Mike bring us certainty in this uncertain world and provides us the answer to the burning question we seek every single day... Mike, is it Friday yet?",
+    "Mike's t-shirt based calendar is more reliable than any calendar in the history of mankind. Our community relies on Mike's YODA t-shirt to show us the way and reduce our anxiety levels. Mike brings us certainty in this uncertain world and he provides us the answer to the burning question we seek every single day... Hey Mike, is it Friday yet?",
 };
 
 export default function RootLayout({
