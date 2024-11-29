47996d13c526e20fe2c39aa82ff2750af905aff0
Rob Pataki <rob.pataki@icloud.com>
2024-11-29 16:49:25 +0000

    Add metadata to the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 
-/* export const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Hey Mike, is it Friday yet?",
   description:
     "Mike's t-shirt based calendar is more reliable than any calendar in the history of mankind. Our community relies on Mike's YODA t-shirt to show us the way and reduce our anxiety levels. Mike bring us certainty in this uncertain world and provides us the answer to the burning question we seek every single day... Mike, is it Friday yet?",
-}; */
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,13 @@
-"use client";
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode } from "react";
 
-import { data } from "../data/data";
-import Layout from "../app/layout";
-import AsciiArtFrame from "@/components/AsciiArtFrame/AsciiArtFrame";
-import { Outcome } from "@/components/Outcome/Outcome";
-
-type Artwork = { photo: string; id: string };
-
-const isItFriday = new Date().getDay() === 5;
-
-// Gather artwork from the data
-const { days: daysData } = data;
-const artworks: Artwork[] = [];
-daysData.forEach((day) => {
-  const { photos } = day;
-  photos.forEach((photo) => {
-    artworks.push({ photo: photo.text, id: day.id });
-  });
-});
-
-const outcomeText = isItFriday ? "YES" : "NO";
-const mixedArtworks = [...artworks].sort(() => Math.random() - 0.5);
-const mixedFridayArtworks = mixedArtworks.filter(
-  (artwork) => artwork.id === "friday"
-);
-const mixedNonFridayArtworks = mixedArtworks.filter(
-  (artwork) => artwork.id !== "friday"
-);
-const todaysArtworks = isItFriday
-  ? mixedFridayArtworks
-  : mixedNonFridayArtworks;
+import Layout from "./layout";
+import { ArtGallery } from "@/components/ArtGallery/ArtGallery";
 
 export default function Page() {
-  const [todaysArtwork, setTodaysArtwork] = useState<Artwork | undefined>();
-  const artworkIndex = useRef(0);
-
-  const rotateArtwork = () => {
-    artworkIndex.current =
-      artworkIndex.current < todaysArtworks.length - 1
-        ? artworkIndex.current + 1
-        : 0;
-    setTodaysArtwork(todaysArtworks[artworkIndex.current]);
-  };
-
-  if (!todaysArtwork) {
-    setTodaysArtwork(todaysArtworks[artworkIndex.current]);
-  }
-
   return (
     <>
       <h1 className="sr-only">Hey Mike, is it Friday yet?</h1>
-      <Outcome isPositive={isItFriday}>{outcomeText}</Outcome>
-      {todaysArtwork ? (
-        <AsciiArtFrame
-          asciiArt={todaysArtwork?.photo}
-          onClick={rotateArtwork}
-        />
-      ) : null}
+      <ArtGallery />
     </>
   );
 }
