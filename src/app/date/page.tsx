487ba2f36499f94513d6400f876df4b5285cc560
Rob Pataki <rob.pataki@icloud.com>
2024-12-09 09:44:10 +0000

    Add forced dynamic rendering (SSR)

--- src/app/date/page.tsx
+++ src/app/date/page.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from "react";
+// Importing dynamic will force Next to render this page on each request (SSR)
+export const dynamic = "force-dynamic";
+
 import Layout from "@/app/layout";
 import { LocaleDate } from "@/components/LocaleDate/LocaleDate";
 
